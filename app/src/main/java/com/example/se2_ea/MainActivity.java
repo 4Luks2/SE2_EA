9231f63ad8d2e573537ecb0bc8add34df3f08e6b
Luks <Lukastroh@edu.aau.at>
2023-03-14 21:43:35 +0000

    filterPrime method and helper methods work now

--- app/src/main/java/com/example/se2_ea/MainActivity.java
+++ app/src/main/java/com/example/se2_ea/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.net.*;
 import java.util.ArrayList;
 
@@ -18,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText etUserInput;
     private TextView tvReturnData;
     private Button btnSend;
+    private Button btnPrime;
     private String serverName = "se2-isys.aau.at";            //"se2-isys.aau.at"; "time.nist.gov";
     private int serverPort = 53212;               //53212; 13;
 
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
         etUserInput = findViewById(R.id.etUserInput);
         tvReturnData = findViewById(R.id.tvReturnData);
         btnSend = findViewById(R.id.btnSend);
+        btnPrime = findViewById(R.id.btnPrime);
 
     }
 
     public void onClickSendToServer(View view) {
-
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -71,35 +71,61 @@ public class MainActivity extends AppCompatActivity {
     public void onClickFilterPrime(View view) {
         String userInput = etUserInput.getText().toString();
 
-        char[] numbers = userInput.toCharArray();
+        System.out.println(userInput);
+
+        char[] charArray = userInput.toCharArray();
         ArrayList<Integer> primeNumbers = new ArrayList<>(); //= Integer.parseInt(String.valueOf(charArray));
 
-        for (char c : numbers) {
-            if(isPrime(c)){
-                primeNumbers.add((int) c);
+        for (int i = 0; i < charArray.length; i++) {
+            System.out.println(charToInt(charArray[i]));
+            if (isPrime(charToInt(charArray[i]))){
+                System.out.println(charToInt(charArray[i]) + "is prime");
+                primeNumbers.add(charToInt(charArray[i]));
+            }else{
+                System.out.println(charToInt(charArray[i]) + "is not prime");
             }
         }
 
-        tvReturnData.setText(toInt(primeNumbers));
+        System.out.println(arrayListToInt(primeNumbers));
+/*
+        for (char c : charArray) {
+            if(isPrime(charToInt(c))){
+                primeNumbers.add(charToInt(c));
+            }
+        }
+*/
+        int retVal = arrayListToInt(primeNumbers);
+        tvReturnData.setText(userInput);
+
+
+
     }
 
     private boolean isPrime(int x) {
+        System.out.println(x + " are u prime?");
         if (x <= 1) {
+            System.out.println("no");
             return false;
         }
         for (int i = 2; i <= Math.sqrt(x); i++) {
             if (x % i == 0) {
+                System.out.println("no");
                 return false;
             }
         }
+        System.out.println("yes");
         return true;
     }
 
-    int toInt(ArrayList<Integer> list) {
+    int arrayListToInt(ArrayList<Integer> list) {
         int retVal = 0;
         for (Integer i : list) { // assuming list is of type List<Integer>
             retVal = 10*retVal + i;
         }
         return retVal;
+    }
+
+    int charToInt(char c){
+        return c - '0';
     }
 }
